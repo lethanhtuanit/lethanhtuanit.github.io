2a743a96e43b29fc78c170e85e3d61e5908abba2
lethanhtuanit <tuan992992@gmail.com>
2019-11-26 22:31:52 +0700

    new

--- New folder/crip.js
+++ New folder/crip.js
@@ -1,38 +1,37 @@
+var elMenu = document.getElementsByClassName('menu')[0];
+var btnTop = document.getElementsByClassName('back-to-top')[0];
+btnTop.addEventListener('click', function() {
+	scrollToTop(1000)
+});
+
 window.onscroll = function() {
 	var sTop = window.pageYOffset;
-	var elMenu = document.getElementsByClassName('menu')[0];
-	var btnTop = document.getElementsByClassName('back-to-top')[0];
+	
 	//var btnTop = $('.back-to-top')[0];
 	//console.log(sTop);
 	if(sTop>20){
 		elMenu.classList.add('co-dinh-menu');
-		//btnTop.classList.add('hien-ra');
+		btnTop.classList.add('hien-ra');
 		// $('.back-to-top').fadeIn();
 	} else {
 		elMenu.classList.remove('co-dinh-menu');
-		//btnTop.classList.remove('hien-ra');
+		btnTop.classList.remove('hien-ra');
 		// $('.back-to-top').fadeOut();
 	}
+};
+function scrollToTop(scrollDuration) {
+const   scrollHeight = window.scrollY,
+        scrollStep = Math.PI / ( scrollDuration / 15 ),
+        cosParameter = scrollHeight / 2;
+var     scrollCount = 0,
+        scrollMargin,
+        scrollInterval = setInterval( function() {
+            if ( window.scrollY != 0 ) {
+                scrollCount = scrollCount + 1;  
+                scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+                window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
+            } 
+            else clearInterval(scrollInterval); 
+        }, 15 );
 }
-/*
-$(document).ready(function() {
-	$(window).scroll(function(event){
-		var pos_body = $('html,body').scrollTop();
-		//console.log(pos-body);
-		if(pos_body>20){
-			$('.menu').addClass('co-dinh-menu');
-		}
-		else {
-			$('.menu').removeClass('co-dinh-menu');
-		}
-		if(pos_body>1200){
-			$('.back-to-top').addClass('hien-ra');
-		}
-		else{
-			$('.back-to-top').removeClass('hien-ra');
-		}
-	});
-	$('.back-to-top').click(function(envent){
-		$('html,body').animate({screenTop:0},1000);
-	});
-});*/
+
